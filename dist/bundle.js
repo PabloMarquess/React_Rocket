bb7f40ec71a2f453e370ce108d52e0c337422231
PabloMarquess <47582611+PabloMarquess@users.noreply.github.com>
2022-05-03 10:18:37 -0300

    Add files via upload

---
+++ dist/bundle.js
@@ -0,0 +1,32 @@
+/*
+ * ATTENTION: The "eval" devtool has been used (maybe by default in mode: "development").
+ * This devtool is neither made for production nor for readable output files.
+ * It uses "eval()" calls to create a separate source file in the browser devtools.
+ * If you are trying to read the output file, select a different devtool (https://webpack.js.org/configuration/devtool/)
+ * or disable the default devtool with "devtool: false".
+ * If you are looking for production-ready output files, see mode: "production" (https://webpack.js.org/configuration/mode/).
+ */
+/******/ (() => { // webpackBootstrap
+/******/ 	var __webpack_modules__ = ({
+
+/***/ "./src/index.jsx":
+/*!***********************!*\
+  !*** ./src/index.jsx ***!
+  \***********************/
+/***/ (() => {
+
+eval("throw new Error(\"Module build failed (from ./node_modules/babel-loader/lib/index.js):\\nSyntaxError: C:\\\\Users\\\\marco\\\\EstudoReact\\\\src\\\\index.jsx: Support for the experimental syntax 'jsx' isn't currently enabled (4:8):\\n\\n\\u001b[0m \\u001b[90m 2 |\\u001b[39m \\u001b[36mimport\\u001b[39m {\\u001b[33mApp\\u001b[39m} \\u001b[36mfrom\\u001b[39m  \\u001b[32m'./App'\\u001b[39m\\u001b[0m\\n\\u001b[0m \\u001b[90m 3 |\\u001b[39m\\u001b[0m\\n\\u001b[0m\\u001b[31m\\u001b[1m>\\u001b[22m\\u001b[39m\\u001b[90m 4 |\\u001b[39m render(\\u001b[33m<\\u001b[39m\\u001b[33mApp\\u001b[39m\\u001b[33m/\\u001b[39m\\u001b[33m>\\u001b[39m\\u001b[33m,\\u001b[39m document\\u001b[33m.\\u001b[39mgetElementById(\\u001b[32m'root'\\u001b[39m))\\u001b[0m\\n\\u001b[0m \\u001b[90m   |\\u001b[39m        \\u001b[31m\\u001b[1m^\\u001b[22m\\u001b[39m\\u001b[0m\\n\\nAdd @babel/preset-react (https://github.com/babel/babel/tree/main/packages/babel-preset-react) to the 'presets' section of your Babel config to enable transformation.\\nIf you want to leave it as-is, add @babel/plugin-syntax-jsx (https://github.com/babel/babel/tree/main/packages/babel-plugin-syntax-jsx) to the 'plugins' section to enable parsing.\\n    at instantiate (C:\\\\Users\\\\marco\\\\EstudoReact\\\\node_modules\\\\@babel\\\\parser\\\\lib\\\\index.js:72:32)\\n    at constructor (C:\\\\Users\\\\marco\\\\EstudoReact\\\\node_modules\\\\@babel\\\\parser\\\\lib\\\\index.js:358:12)\\n    at Parser.raise (C:\\\\Users\\\\marco\\\\EstudoReact\\\\node_modules\\\\@babel\\\\parser\\\\lib\\\\index.js:3335:19)\\n    at Parser.expectOnePlugin (C:\\\\Users\\\\marco\\\\EstudoReact\\\\node_modules\\\\@babel\\\\parser\\\\lib\\\\index.js:3392:18)\\n    at Parser.parseExprAtom (C:\\\\Users\\\\marco\\\\EstudoReact\\\\node_modules\\\\@babel\\\\parser\\\\lib\\\\index.js:12970:18)\\n    at Parser.parseExprSubscripts (C:\\\\Users\\\\marco\\\\EstudoReact\\\\node_modules\\\\@babel\\\\parser\\\\lib\\\\index.js:12540:23)\\n    at Parser.parseUpdate (C:\\\\Users\\\\marco\\\\EstudoReact\\\\node_modules\\\\@babel\\\\parser\\\\lib\\\\index.js:12519:21)\\n    at Parser.parseMaybeUnary (C:\\\\Users\\\\marco\\\\EstudoReact\\\\node_modules\\\\@babel\\\\parser\\\\lib\\\\index.js:12490:23)\\n    at Parser.parseMaybeUnaryOrPrivate (C:\\\\Users\\\\marco\\\\EstudoReact\\\\node_modules\\\\@babel\\\\parser\\\\lib\\\\index.js:12284:61)\\n    at Parser.parseExprOps (C:\\\\Users\\\\marco\\\\EstudoReact\\\\node_modules\\\\@babel\\\\parser\\\\lib\\\\index.js:12291:23)\");\n\n//# sourceURL=webpack://estudoreact/./src/index.jsx?");
+
+/***/ })
+
+/******/ 	});
+/************************************************************************/
+/******/ 	
+/******/ 	// startup
+/******/ 	// Load entry module and return exports
+/******/ 	// This entry module doesn't tell about it's top-level declarations so it can't be inlined
+/******/ 	var __webpack_exports__ = {};
+/******/ 	__webpack_modules__["./src/index.jsx"]();
+/******/ 	
+/******/ })()
+;
